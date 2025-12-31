294cfb8977cb53654eccda208538a5b65e4f72ca
DarkoHristovski <hristovski15@gmail.com>
2025-12-31 13:41:37 +0100

    creating section about latest games

---
+++ client/src/services/gameServices.js
@@ -0,0 +1,19 @@
+
+const baseUrl='http://localhost:3030';
+
+/*
+expoert const getAll = () =>{
+    return fetch(`${baseUrl}/data/games?`)
+    .then(result=>resultjson());
+}
+    */
+
+export const getAll = async() =>{
+    const getData = await fetch(`${baseUrl}/data/games?`);
+    const result = await getData.json();
+    return result;
+
+
+}
+
+
